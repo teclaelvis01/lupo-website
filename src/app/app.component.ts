9df92ce93264f4f91c9f85200dc223f74ad52301
Elvis Reyes <teclaelvis01@gmail.com>
2023-10-17 13:36:29 +0000

    style: :art: configure theme

---
+++ src/app/app.component.ts
@@ -0,0 +1,142 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+// import $ from 'jquery';
+declare var $: any;
+// import '../jquery.pagepiling.d.ts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'lupo vidal';
+
+  ngAfterViewInit(): void {
+    $(window).on('load', function () {
+      console.log('window loaded');
+      $('.loader').fadeOut(200);
+      $('.line').addClass('active');
+    });
+    $(document).ready(() => {
+
+      /* Full page scroll*/
+      console.log('DODUMENT READY');
+      if ($('#pagepiling').length > 0) {
+        console.log('pagepiling OKOKOKOKOKOKOKOOKOKO');
+        $('#pagepiling').pagepiling({
+          scrollingSpeed: 280,
+          navigation: false,
+          menu: '.navbar-nav',
+          anchors: ['home', 'about', 'experience', 'skills', 'projects', 'partners', 'testimonials', 'news', 'contact'],
+          afterRender: function (anchorLink: any, index: any) {
+            NavbarColor();
+
+          },
+          afterLoad: function (anchorLink: any, index: any) {
+            $('.pp-section .intro').removeClass('animate');
+            $('.active .intro').addClass('animate');
+            NavbarColor();
+          }
+        });
+
+
+
+        function NavbarColor() {
+          if ($('.pp-section.active').hasClass('navbar-is-white')) {
+            $('.navbar-desctop').addClass('navbar-white');
+            $('.progress-nav').addClass('progress-nav-white');
+            $('.navbar-bottom').addClass('navbar-bottom-white');
+          }
+          else {
+            $('.navbar-desctop').removeClass('navbar-white');
+            $('.progress-nav').removeClass('progress-nav-white');
+            $('.navbar-bottom').removeClass('navbar-bottom-white');
+          }
+        }
+      }
+    });
+
+  }
+
+  ngOnInit(): void {
+
+    /* Navbar toggler */
+    $('.toggler').on('click', function () {
+      $('body').addClass('menu-is-open');
+    });
+
+    $('.close, .click-capture').on('click', function () {
+      $('body').removeClass('menu-is-open');
+    });
+
+
+    /* Navbar mobile */
+    $('.navbar-nav-mobile li a').on('click', () => {
+      $('body').removeClass('menu-is-open');
+      $('.navbar-nav-mobile li a').removeClass('active');
+      $(this).addClass('active');
+    });
+
+    $('.popup-youtube').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
+
+
+
+    /* Change bacgkround on project section*/
+    $('.project-box').on('mouseover', () => {
+      var index = $('.project-box').index(this);
+      $('.bg-changer .section-bg').removeClass('active').eq(index).addClass('active');
+    });
+
+
+    /* Carousel experience*/
+    // $('.carousel-experience').owlCarousel({
+    //   loop: true,
+    //   margin: 45,
+    //   dots: true,
+    //   nav: true,
+    //   smartSpeed: 1000,
+    //   items: 1
+    // });
+
+    /* Carousel testimonials */
+    // $('.carousel-testimonials').owlCarousel({
+    //   loop: true,
+    //   margin: 10,
+    //   nav: true,
+    //   dots: false,
+    //   items: 1
+    // });
+
+    //   /* Send form */
+    // if ($('.js-ajax-form').length) {
+    // 	$('.js-ajax-form').each(function(){
+    // 		$(this).validate({
+    // 			errorClass: 'error',
+    // 		    submitHandler: function(form){
+    // 	        	$.ajax({
+    // 		            type: "POST",
+    // 		            url:"mail.php",
+    // 		            data: $(form).serialize(),
+    // 		            success: function() {
+    // 	                	$('#success-message').show();
+    // 	                },
+
+    // 	                error: function(){
+    // 	                	$('#error-message').show();
+    // 		            }
+    // 		        });
+    // 		    }
+    // 		});
+    // 	});
+    // }
+
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  yearsExperience: number = 0;
+  
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+
+
+
+  
+
+}
