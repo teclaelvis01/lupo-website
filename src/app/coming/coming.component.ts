6ae10cbebaa267ca12c34245e2d9f53b6753da31
Elvis Reyes <teclaelvis01@gmail.com>
2023-10-18 14:14:22 +0000

    style: :art: Coming soon 2
    Add coming soon page

--- src/app/coming/coming.component.ts
+++ src/app/coming/coming.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -6,26 +6,38 @@ declare var $: any;
   templateUrl: './coming.component.html',
   styleUrls: ['./coming.component.css']
 })
-export class ComingComponent implements OnInit {
+export class ComingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
+
     if ($.isFunction($.fn.fluidbox)) {
       $('a').fluidbox();
     }
 
-    //ROUNDED TIMES COUNTDOWN
-
     if (this.isExists('#rounded-countdown')) {
-      var remainingSec = $('.countdown').data('remaining-sec');
+      const now = $.now();
+      const endDate = new Date('2023-12-25 00:00:00');
+      const endTime = endDate.getTime();
+      const difference = endTime - now;
+
+
       $('.countdown').ClassyCountdown({
         theme: "flat-colors-very-wide",
-        end: $.now() + remainingSec
+        end: now + difference / 1000, // in seconds
+        now: now,
+        labelsOptions: {
+          lang: {
+            days: 'Días',
+            hours: 'Horas',
+            minutes: 'Minutos',
+            seconds: 'Segundos'
+          }
+        },
+        // end: $.now() + dateEnd.getTime()
+        // end: dateEnd.getTime()
       });
-      console.log('rounded-countdown OKOKOKOKOKOKOKOOKOKO');
     }
 
-    //NORMAL TIMES COUNTDOWN
-    
     if (this.isExists('#normal-countdown')) {
       var date = $('#normal-countdown').data('date');
       $('#normal-countdown').countdown(date, (event: any) => {
@@ -36,11 +48,6 @@ export class ComingComponent implements OnInit {
           + '<div class="time-sec"><h3 class="main-time">%S</h3> <span>Sec</span></div>'));
       });
     }
-
-
-
-
-    // COUNTDOWN TIME 
 
     this.countdownTime();
 
@@ -63,14 +70,13 @@ export class ComingComponent implements OnInit {
 
     });
 
-    // Circular Progress Bar
 
-    var isAnimated = false;
 
   }
 
   ngOnInit(): void {
-
+    // change the title of the page
+    document.title = "Coming Soon";
   }
 
 
@@ -87,7 +93,8 @@ export class ComingComponent implements OnInit {
       });
     }
   }
-  dropdownMenu(winWidth: any) {
+
+  dropdownMenu(winWidth: number) {
 
     if (winWidth > 767) {
 
@@ -123,6 +130,159 @@ export class ComingComponent implements OnInit {
     }
     return false;
   }
+
+  // initMap() {
+
+  // 	// Create a new StyledMapType object, passing it an array of styles,
+  // 	// and the name to be displayed on the map type control.
+  // 	var styledMapType = new google.maps.StyledMapType(
+  // 	[
+  // 		{
+  // 			"featureType": "administrative",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"saturation": "-100"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "administrative.province",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"visibility": "off"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "landscape",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"saturation": -100
+  // 				},
+  // 				{
+  // 					"lightness": 65
+  // 				},
+  // 				{
+  // 					"visibility": "on"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "poi",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"saturation": -100
+  // 				},
+  // 				{
+  // 					"lightness": "50"
+  // 				},
+  // 				{
+  // 					"visibility": "simplified"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "road",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"saturation": "-100"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "road.highway",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"visibility": "simplified"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "road.arterial",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"lightness": "30"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "road.local",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"lightness": "40"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "transit",
+  // 			"elementType": "all",
+  // 			"stylers": [
+  // 				{
+  // 					"saturation": -100
+  // 				},
+  // 				{
+  // 					"visibility": "simplified"
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "water",
+  // 			"elementType": "geometry",
+  // 			"stylers": [
+  // 				{
+  // 					"hue": "#ffff00"
+  // 				},
+  // 				{
+  // 					"lightness": -25
+  // 				},
+  // 				{
+  // 					"saturation": -97
+  // 				}
+  // 			]
+  // 		},
+  // 		{
+  // 			"featureType": "water",
+  // 			"elementType": "labels",
+  // 			"stylers": [
+  // 				{
+  // 					"lightness": -25
+  // 				},
+  // 				{
+  // 					"saturation": -100
+  // 				}
+  // 			]
+  // 		}
+  // 	],
+  // 		{name: 'Styled Map'});
+
+  // 	// Create a map object, and include the MapTypeId to add
+  // 	// to the map type control.
+
+  // 	var uluru = {lat: 56.946285, lng: 24.105078};
+  //     var map = new google.maps.Map(document.getElementById('map'), {
+  // 		zoom: 4,
+  // 		center: uluru
+  //     });
+
+  // 	var image = 'images/google-marker.png';
+  // 	var marker = new google.maps.Marker({
+  // 		position: uluru,
+  // 		map: map,
+  // 		icon: image
+  // 	});
+  // 	//Associate the styled map with the MapTypeId and set it to display.
+  // 	map.mapTypes.set('styled_map', styledMapType);
+  // 	map.setMapTypeId('styled_map');
+  // }
+
 
 
 }
